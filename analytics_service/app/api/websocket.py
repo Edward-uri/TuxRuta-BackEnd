ad8079c66470549cb97a2e7d31400daf367462bd
sullentx <233290@ids.upchiapas.edu.mx>
2025-07-23 21:58:04 -0600

    websocket ready for testing

---
+++ analytics_service/app/api/websocket.py
@@ -0,0 +1,30 @@
+from fastapi import WebSocket
+import json
+
+class WebSocketManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, data: dict):
+        message = json.dumps(data)
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = WebSocketManager()
+
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except Exception:
+        pass
+    finally:
+        await manager.disconnect(websocket)
